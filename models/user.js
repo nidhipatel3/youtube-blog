b20f4febddfddf2a3990e2004fc68c9fce6bfe15
nidhipatel3 <jbsnidhipatel14@gmail.com>
2025-03-03 17:00:47 -0500

    Worked on signin and signup page Design signin and signup page worked on hash password created route for signin and signup

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+const { createHmac, randomBytes } = require('node:crypto');
 
 const userSchema = new Schema(
     {
@@ -31,6 +32,39 @@ const userSchema = new Schema(
     },
     { timestamp: true }
 );
+
+//using pre middleware of mangoose
+userSchema.pre('save', function () {
+    const user = this;
+    if (!user.isModified("password")) return;
+    const salt = randomBytes(16).toString(); //random string
+    const hashedPassword = createHmac('sha256', salt)
+        .update(user.password)
+        .digest("hex");
+    this.salt = salt;
+    this.password = hashedPassword;
+
+})
+
+//making function
+userSchema.static('matchPassword', async function (email, password) {
+    const user = await this.findOne({ email });
+    if (!user) throw new Error('User not found !');
+
+    console.log(user);
+    const salt = user.salt;
+    const hashedPassword = user.password;
+
+    const userProvideHash = createHmac("sha256", salt)
+        .update(password)
+        .digest("hex")
+
+    if (hashedPassword !== userProvideHash) {
+        throw new Error("Incorrect Password!");
+    }
+    return user;
+
+})
 
 const User = model('user', userSchema)
 
