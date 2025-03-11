5510214d43b5c40c8b86b42f7d72d7704938a05b
nidhipatel3 <jbsnidhipatel14@gmail.com>
2025-03-11 13:20:13 -0400

    Added comments functionality Design blog view page

--- routes/blog.js
+++ routes/blog.js
@@ -3,6 +3,7 @@ const multer = require('multer')
 const path = require('path')
 const router = Router();
 const Blog = require('../models/blog')
+const Comment = require('../models/comments');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -30,6 +31,27 @@ router.post('/', upload.single('coverImage'), async (req, res) => {
         coverImageURL: `/uploads/${req.file.filename}`
     })
     return res.redirect(`/blog/${blog._id}`)
+})
+
+router.get('/:id', async (req, res) => {
+    const blog = await Blog.findById(req.params.id).populate("createdBy");
+    const comments = await Comment.find({ blogId: req.params.id }).populate("createdBy")
+    console.log("blog: ", blog)
+    return res.render('blog', {
+        user: req.user,
+        blog,
+        comments,
+    })
+})
+
+router.post('/comment/:blogId', async (req, res) => {
+    await Comment.create({
+        content: req.body.content,
+        blogId: req.params.blogId,
+        createdBy: req.user._id,
+    })
+    return res.redirect(`/blog/${req.params.blogId}`)
+
 })
 
 module.exports = router;
